47ef540c0f35df26caa8a0a70161ad6614740e2a
Ramazan <romagt2015@mail.ru>
2024-04-01 20:27:04 +0300

    jov

--- vue-modeling/src/components/Myhello.vue
+++ vue-modeling/src/components/Myhello.vue
@@ -1,13 +1,45 @@
+<!--
+<script>
+export default{
+    data(){
+        return{
+            count: 0
+        }
+    },
+    methods:{
+        increment(){
+            this.count++;
+        }
+    }
+}
+</script>
+-->
+<script setup>
+
+import {ref} from 'vue'
+
+const count = ref(0)
+
+function increment(){
+    count.value++
+}
+
+</script>
+
 
 
 <template>
 
-<p> Это мой компонент </p>
-<button>choko</button>
+<h1>{{ count }}</h1>
+<button @click="increment">+</button>
 
 
 
 </template>
+
+<style scoped> /*scoped */
+
+</style>
 
 
  
