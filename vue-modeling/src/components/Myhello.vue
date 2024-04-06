bda89b1d53aca461f17d9a2d11e0a4e9de49d1d5
Ramazan <romagt2015@mail.ru>
2024-04-06 15:38:37 +0300

    Update Myhello.vue

--- vue-modeling/src/components/Myhello.vue
+++ vue-modeling/src/components/Myhello.vue
@@ -1,38 +1,13 @@
-<!--
-<script>
-export default{
-    data(){
-        return{
-            count: 0
-        }
-    },
-    methods:{
-        increment(){
-            this.count++;
-        }
-    }
-}
-</script>
--->
+
 <script setup>
 
-import {ref} from 'vue'
 
-const count = ref(0)
-
-function increment(){
-    count.value++
-}
 
 </script>
 
 
 
 <template>
-
-<h1>{{ count }}</h1>
-<button @click="increment">+</button>
-
 
 
 </template>
