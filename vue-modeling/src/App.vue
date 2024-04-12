be1d05897517c15307eb156e7a2935a7e783b332
Ramazan <romagt2015@mail.ru>
2024-04-12 12:36:06 +0300

    Header

--- vue-modeling/src/App.vue
+++ vue-modeling/src/App.vue
@@ -1,7 +1,40 @@
 <template>
   
 
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl">Archakov blog</div>
+  <div class="lok bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    
+  
+  
+  <header class="flex">
+    <div>
+      <img src="/public/logo.png" alt="">
+      <div>
+        <h2>Vue-modeling</h2>
+        <p>Магазин лучших 3D моделей</p>
+      </div>
+    </div>
+
+    <ul>
+      <li>
+        <img src="public/cart.svg" alt="">
+        <b>1205руб</b>
+      </li>
+      <li>
+        <img src="public/heart.svg" alt="">
+        <b>Закладки</b>
+      </li>
+      <li>
+        <img src="public/heart.svg" alt="">
+        <b>Профиль</b>
+      </li>
+    </ul>
+
+
+  </header>
+  
+
+  <h1>JED STUDIO</h1>
+  </div>
   
 
 </template>
