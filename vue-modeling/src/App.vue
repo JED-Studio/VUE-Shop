293e98355af1a2fc0e97c355d1234f0a90dded3b
Ramazan <romagt2015@mail.ru>
2024-04-12 14:01:28 +0300

    import

--- vue-modeling/src/App.vue
+++ vue-modeling/src/App.vue
@@ -2,16 +2,16 @@
   
 
   <div class="lok bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-  <header />
+  <Header />
   <h1>JED STUDIO</h1>
-  <Myhello />
+  
   </div>
   
 
 </template>
 
 <script setup>
-import header from './components/header.vue'
+import Header from './components/Header.vue';
 import Myhello from './components/Myhello.vue'
 
 </script>
