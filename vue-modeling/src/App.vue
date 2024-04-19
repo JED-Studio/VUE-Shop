d35af8bf279980500a842d3378ee4a902ad97819
Ramazan <romagt2015@mail.ru>
2024-04-19 20:47:12 +0300

    watch

--- vue-modeling/src/App.vue
+++ vue-modeling/src/App.vue
@@ -7,11 +7,11 @@
   <div class="p-10">
     <div class="flex justify-between">
       <h2 class="text-3xl font-bold mb-8">Все модели</h2>
-<div>
-      <select class="py-2 px-3 border rounded-md outline-none">
-        <option>По названию</option>
-        <option>По цене (дешевые)</option>
-        <option>По цене (дорогие)</option>
+<div class="flex gap-4">
+      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+        <option value="name">По названию</option>
+        <option value="price">По цене (дешевые)</option>
+        <option value="-price">По цене (дорогие)</option>
       </select>
 
       <div class="relative">
@@ -21,18 +21,131 @@
     
     </div>
 </div>
-    <CardList />
+
+<div class="mt-10">
+    <CardList :items="items" />
+</div>
+
   </div>
 </div>
 
 </template>
 
 <script setup>
+import { onMounted, reactive, ref, watch} from 'vue';
+import axios from 'axios'
 
 import Drawer from './components/Drawer.vue';
 import CardList from './components/CardList.vue'
 import Header from './components/Header.vue';
 import Myhello from './components/Myhello.vue'
 
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy:'',
+  serchQuery:'',
+})
+
+
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+onMounted(async() => {
+  try {
+   const {data} = await axios.get('https://098426db1591c9bb.mokky.dev/items')
+   items.value = data;
+  } catch (err){
+  console.log(err)}
+})
+
+watch(filters, async() => {
+  try {
+   const {data} = await axios.get('https://098426db1591c9bb.mokky.dev/items?sortBy='+ filters.sortBy)
+   items.value = data;
+  } catch (err){
+  console.log(err)
+}
+
+})
+
+/*const items = [
+    {
+        "id": 1,
+        "title": "Мужские Кроссовки Nike Blazer Mid Suede",
+        "price": 12999,
+        "imageUrl": "/sneakers/sneakers-1.jpg"
+    },
+    {
+        "id": 2,
+        "title": "Мужские Кроссовки Nike Air Max 270",
+        "price": 15600,
+        "imageUrl": "/sneakers/sneakers-2.jpg"
+    },
+    {
+        "id": 3,
+        "title": "Мужские Кроссовки Nike Blazer Mid Suede",
+        "price": 8499,
+        "imageUrl": "/sneakers/sneakers-3.jpg"
+    },
+    {
+        "id": 4,
+        "title": "Кроссовки Puma X Aka Boku Future Rider",
+        "price": 7800,
+        "imageUrl": "/sneakers/sneakers-4.jpg"
+    },
+    {
+        "id": 5,
+        "title": "Кроссовки Future Rider",
+        "price": 9550,
+        "imageUrl": "/sneakers/sneakers-5.jpg"
+    },
+    {
+        "id": 6,
+        "title": "Кроссовки Black Edition",
+        "price": 16999,
+        "imageUrl": "/sneakers/sneakers-6.jpg"
+    },
+    {
+        "id": 7,
+        "title": "Кроссовки Orange Boomb Edition",
+        "price": 7499,
+        "imageUrl": "/sneakers/sneakers-7.jpg"
+    },
+    {
+        "id": 8,
+        "title": "Кроссовки Nike Air Max 270",
+        "price": 15600,
+        "imageUrl": "/sneakers/sneakers-8.jpg"
+    },
+    {
+        "id": 9,
+        "title": "Кроссовки Nike Air Force 1",
+        "price": 5900,
+        "imageUrl": "/sneakers/sneakers-9.jpg"
+    },
+    {
+        "id": 10,
+        "title": "Кроссовки Adidas Ultraboost",
+        "price": 11500,
+        "imageUrl": "/sneakers/sneakers-10.jpg"
+    },
+    {
+        "id": 11,
+        "title": "Кроссовки Puma Clyde All-Pro",
+        "price": 7600,
+        "imageUrl": "/sneakers/sneakers-11.jpg"
+    },
+    {
+        "id": 12,
+        "title": "Кроссовки Converse Chuck Taylor All-Star",
+        "price": 13000,
+        "imageUrl": "/sneakers/sneakers-12.jpg"
+    }
+]
+*/
 </script>
 
--- vue-modeling/src/components/CardList.vue
+++ vue-modeling/src/components/CardList.vue
@@ -1,14 +1,22 @@
 <script setup>
 import Card from './Card.vue';
-</script>
 
+
+
+defineProps({
+  items: Array
+})
+</script>
 
 
 <template>
     <div class="grid grid-cols-4 gap-5">
-  <Card title="Мужские кроссвки Nike Blazer Mid Suede" 
-  imageUrl="/public/sneakers/sneakers-1.jpg" 
-  :price="1000" 
+  <Card 
+  v-for="item in items"
+  :key="item.id"
+  :title="item.title" 
+  :imageUrl="item.imageUrl" 
+  :price="item.price" 
   :isFavorite="false" 
   :onClikAdd="onClikAdd" 
   :isAdded="true"/>
